5fe14c639273d6dae0ece9c8af1527eca6bb3b73
Raj yadav <107810956+rajyadav98800@users.noreply.github.com>
2024-02-13 11:10:02 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings: ['Frontend Developer.', 'Gamer.','3D - VFX Designer.' ,'&amp; Web Developer.'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop:true
+});
