0809aaa1d17ef3cade35774a1ecde6cb3a2dcda3
Raj yadav <107810956+rajyadav98800@users.noreply.github.com>
2024-02-15 16:22:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text",{
-    strings: ['Frontend Developer.', 'Gamer.','3D - VFX Designer.' ,'&amp; Web Developer.'],
+    strings: ['Frontend Developer.', 'Gamer.','3D - VFX Designer.' ,'amp; Web Developer.'],
     typeSpeed: 50,
     backSpeed: 50,
     backDelay: 1000,
